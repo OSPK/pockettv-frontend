ab9122cc1660d25fea8acacd5f9128c16f583e80
OSPK <waqas@opensource.com.pk>
2015-11-30 22:32:13 +0500

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,6 @@ if ($(window).width()>768) {
     var channel_landing_height = $("#channel_landing .player").height();
     $(".player_container, #channel_landing .listing").height(channel_landing_height);
 };
-alert($(window).width());
 // jQuery(window).load(function() {
 // });
 
