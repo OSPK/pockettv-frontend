bfe4f86f6f09ba1fa1276e94a2d6bce7d03bbf9b
OSPK <waqas@opensource.com.pk>
2015-11-30 21:12:57 +0500

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,22 +10,22 @@ $(document).ready(function() {
  
       autoPlay: false, //Set AutoPlay to 3 seconds
       pagination: false,
-      items : 5,
-      itemsDesktop : [1199,5],
-      itemsDesktopSmall : [979,4],
-      itemsTablet : [768,3],
-      itemsMobile : [479,2]
+      items : 5.3,
+      itemsDesktop : [1199,5.3],
+      itemsDesktopSmall : [979,4.3],
+      itemsTablet : [768,3.3],
+      itemsMobile : [479,2.3]
  
   });
   $(".chan-slider").owlCarousel({
  
       autoPlay: false, //Set AutoPlay to 3 seconds
       pagination: false,
-      items : 5,
-      itemsDesktop : [1199,5],
-      itemsDesktopSmall : [979,4],
-      itemsTablet : [768,3],
-      itemsMobile : [479,2]
+      items : 5.3,
+      itemsDesktop : [1199,5.3],
+      itemsDesktopSmall : [979,4.3],
+      itemsTablet : [768,3.3],
+      itemsMobile : [479,2.3]
  
   });
  
