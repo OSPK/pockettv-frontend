6e507c4e71545c906248cf89f7c8822bba73a5d1
OSPK <waqas@opensource.com.pk>
2015-11-30 20:55:06 +0500

    up

---
+++ static/js/scripts.js
@@ -0,0 +1,32 @@
+var channel_landing_height = $("#channel_landing .player").height();
+$(".player_container, #channel_landing .listing").height(channel_landing_height);
+
+// jQuery(window).load(function() {
+// });
+
+$(document).ready(function() {
+ 
+  $(".video-slider").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      pagination: false,
+      items : 5,
+      itemsDesktop : [1199,5],
+      itemsDesktopSmall : [979,4],
+      itemsTablet : [768,3],
+      itemsMobile : [479,2]
+ 
+  });
+  $(".chan-slider").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      pagination: false,
+      items : 5,
+      itemsDesktop : [1199,5],
+      itemsDesktopSmall : [979,4],
+      itemsTablet : [768,3],
+      itemsMobile : [479,2]
+ 
+  });
+ 
+});
