9bee88ed0fd7a01c1c59a7559053b9e2751d225f
OSPK <waqas@opensource.com.pk>
2015-12-01 00:24:59 +0500

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,33 +1,38 @@
-if ($(window).width()>768) {
+if ($(window).width() > 768) {
     var channel_landing_height = $("#channel_landing .player").height();
     $(".player_container, #channel_landing .listing").height(channel_landing_height);
 };
-// jQuery(window).load(function() {
-// });
+jQuery(window).load(function() {
+    
+});
 
 $(document).ready(function() {
- 
-  $(".video-slider").owlCarousel({
- 
-      autoPlay: false, //Set AutoPlay to 3 seconds
-      pagination: false,
-      items : 5.3,
-      itemsDesktop : [1199,5.3],
-      itemsDesktopSmall : [979,4.3],
-      itemsTablet : [768,3.3],
-      itemsMobile : [479,2.3]
- 
-  });
-  $(".chan-slider").owlCarousel({
- 
-      autoPlay: false, //Set AutoPlay to 3 seconds
-      pagination: false,
-      items : 5.3,
-      itemsDesktop : [1199,5.3],
-      itemsDesktopSmall : [979,4.3],
-      itemsTablet : [768,3.3],
-      itemsMobile : [479,2.3]
- 
-  });
- 
+    
+    $(".video-slider").owlCarousel({
+
+        autoPlay: false, //Set AutoPlay to 3 seconds
+        pagination: false,
+        navigation: true,
+        navigationText: ["<i class='glyphicon glyphicon-chevron-left'></i>","<i class='glyphicon glyphicon-chevron-right'></i>"],
+        items: 5.3,
+        itemsDesktop: [1199, 5.3],
+        itemsDesktopSmall: [979, 4.3],
+        itemsTablet: [768, 3.3],
+        itemsMobile: [479, 2.3]
+
+    });
+    $(".chan-slider").owlCarousel({
+
+        autoPlay: false, //Set AutoPlay to 3 seconds
+        pagination: false,
+        navigation: true,
+        navigationText: ["",""],
+        items: 5.3,
+        itemsDesktop: [1199, 5.3],
+        itemsDesktopSmall: [979, 4.3],
+        itemsTablet: [768, 3.3],
+        itemsMobile: [479, 2.3]
+
+    });
+
 });
