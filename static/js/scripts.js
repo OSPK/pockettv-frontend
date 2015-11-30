c3145a75a471a036f14355817f940659223e6d38
OSPK <waqas@opensource.com.pk>
2015-11-30 20:32:30 +0500

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,20 @@ $(".player_container, #channel_landing .listing").height(channel_landing_height)
 // });
 
 $(document).ready(function() {
+ 
+  $(".video-slider").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
+
+$(document).ready(function() {
     $('.video-slider').slick({
         centerPadding: '60px',
         slidesToShow: 6,
