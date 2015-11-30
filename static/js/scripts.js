192fe27ccaf691f6943bb332978781b5fb6512e5
OSPK <waqas@opensource.com.pk>
2015-11-30 20:40:24 +0500

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,62 +10,23 @@ $(document).ready(function() {
  
       autoPlay: 3000, //Set AutoPlay to 3 seconds
  
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+      items : 5,
+      itemsDesktop : [1199,5],
+      itemsDesktopSmall : [979,4]
+      itemsTablet : [768,3]
+      itemsMobile : [479,2]
+ 
+  });
+  $(".chan-slider").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 5,
+      itemsDesktop : [1199,5],
+      itemsDesktopSmall : [979,4]
+      itemsTablet : [768,3]
+      itemsMobile : [479,2]
  
   });
  
-});
-
-$(document).ready(function() {
-    
-
-    $('.chan-slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 5,
-        variableWidth: true,
-        draggable: true,
-        infinite: true,
-        responsive: [{
-            breakpoint: 1200,
-            settings: {
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 5
-            }
-        }, {
-            breakpoint: 992,
-            settings: {
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 2
-            }
-        }, {
-            breakpoint: 320,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }]
-    });
 });
